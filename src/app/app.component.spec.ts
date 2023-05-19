9a5a8d33d9730f92e9ea7de10b54c3c3d421c5e8
kk1169 <39980501+kk1169@users.noreply.github.com>
2023-05-19 21:48:55 +0530

    image and home pages
    image and home pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fasty-food'`, () => {
+  it(`should have as title 'practice-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fasty-food');
+    expect(app.title).toEqual('practice-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fasty-food app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('practice-app app is running!');
   });
 });
